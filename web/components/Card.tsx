53146bc120fe15291997a661927ed11f127312f1
SayarB <sayarbhattacharyya03@gmail.com>
2023-08-27 15:31:57 +0530

    feat: added card slide in animation

--- web/components/Card.tsx
+++ web/components/Card.tsx
@@ -3,15 +3,24 @@ import Button from './Button';
 import { FC, ReactNode } from 'react';
 import fameImage from '@/assets/fame.png'
 import { title } from 'process';
+import { motion } from 'framer-motion';
 interface Props {
     children: ReactNode;
-    bg: string
+    bg: string;
+    i: number
 }
-const Card: FC<Props> = ({ children, bg }) => {
+const Card: FC<Props> = ({ children, bg, i }) => {
     return (
-        <div style={{ backgroundColor: bg || "white" }} className='flex-col p-3 m-2 lg:m-4 min-w-[300px] rounded-md border-2 border-black w-[80vw] sm:max-w-[40vw] lg:max-w-[25vw] xl:max-w-[20vw] h-fit'>
+
+        <motion.div
+            initial={{ y: "100%" }}
+            whileInView={{ y: 0 }}
+            transition={{ duration: 0.2, delay: i * 0.05 }}
+            viewport={{ once: true }}
+            style={{ backgroundColor: bg || "white" }} className='flex-col p-3 m-2 lg:m-4 min-w-[300px] rounded-md border-2 border-black w-[75vw] sm:max-w-[40vw] lg:max-w-[25vw] xl:max-w-[20vw] h-fit' >
             {children}
-        </div>
+        </motion.div>
+
     );
 }
 
