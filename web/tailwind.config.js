2962507e731ddfc9bf62cdecdf0054717501d220
anirudh <anirudh04mishra@gmail.com>
2023-09-08 12:22:26 +0530

    feat: footer

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -14,6 +14,8 @@ module.exports = {
       },
       colors: {
         pastel_red: '#EF7167',
+        pastel_blue: '#71A3F5',
+        pastel_green: '#4AB582',
         dark: '#15171B',
         blue: '#4285F4BF',
         green: '#0F9D58BF',
