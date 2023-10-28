e3621e73fa2e3e67510f50ac918e56e23ec4fa1d
anirudh <anirudh04mishra@gmail.com>
2023-10-28 19:06:33 +0530

    feat: social footer

--- web/components/Footer.tsx
+++ web/components/Footer.tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/navigation'
 import React from 'react'
 import { FC } from 'react'
 import { scroller } from 'react-scroll'
+import socials from "@/content/social.json"
+import Icon from './Icons'
 
 interface Props {
     bg?: string
@@ -28,29 +30,32 @@ const Footer: FC<Props> = ({bg}) => {
     <div className={`relative w-[100vw] text-white max-h-fit snap-y bg-${bg}`}>
       {/* add an image sticking to the bottom of this div and with some margin on top */}
       <Image alt='footer' src={"/footer-clouds.svg"} width={200} height={200} className='w-full h-auto pt-8' />
-      <div className='absolute bottom-0 w-full flex flex-col lg:px-32 px-20 lg:gap-20 gap-1 items-center'>
+      <div className='absolute bottom-0 w-full flex flex-col lg:px-32 px-20 xl:gap-14 lg:gap-10 gap-4 items-center'>
         <div className="sm:flex hidden w-full flex-row justify-between gap-5 flex-wrap">
           <div className='flex flex-col gap-2'>
             <Image className='h-8 w-20 mb-3' src={"/newlogowhite.svg"} width={80} height={100} alt='Logo' />
             <p className='pl-2 uppercase'>Google Developer</p>
             <p className='pl-2 uppercase'>Students Clubs</p>
           </div>
-          <div className='uppercase'>
-            <h1 onClick={() => scrollToSection("fame")} className='font-bold w-min mb-5 cursor-pointer'>
+          <div className='flex flex-col xl:gap-4 gap-2 uppercase'>
+            <h1 onClick={() => scrollToSection("fame")} className='font-bold w-min cursor-pointer'>
               Achievements
             </h1>
-            <h1 onClick={() => scrollToSection("events")} className='font-bold w-min mb-5 cursor-pointer'>
+            <h1 onClick={() => scrollToSection("events")} className='font-bold w-min cursor-pointer'>
               Events
             </h1>
-            <h1 onClick={() => scrollToSection("team")} className='font-bold w-min mb-5 cursor-pointer'>
+            <h1 onClick={() => scrollToSection("team")} className='font-bold w-min cursor-pointer'>
               Team
             </h1>
-            <h1 onClick={() => scrollToSection("projects")} className='font-bold w-min mb-5 cursor-pointer'>
+            <h1 onClick={() => scrollToSection("projects")} className='font-bold w-min cursor-pointer'>
               Projects
             </h1>
           </div>
         </div>
-        <p className='py-6 px-3 font-light text-gray-300'>Made with &#128153; by GDSC-VIT</p>
+        <div className='lg:m-0 gap-3 flex justify-between'>
+          {socials.map((social, i) => <Icon small key={"soc" + i} {...social} />)}
+        </div>
+        <p className='pb-6 px-3 font-light text-gray-300'>Made with &#128153; by GDSC-VIT</p>
       </div>
     </div>
   )
--- web/components/Icons/index.tsx
+++ web/components/Icons/index.tsx
@@ -8,12 +8,13 @@ import { getIcon } from "./icons";
 type Props = {
     icon: string,
     link: string,
-    name: string
+    name: string,
+    small?: boolean
 }
 
-const Icon = ({ icon, name, link }: Props) => {
+const Icon = ({ icon, name, link, small }: Props) => {
     return (
-        <Link target="blank" href={link} className="flex items-center w-[3rem] h-[3rem]" >
+        <Link target="blank" href={link} className={`flex items-center ${small ? "w-[2rem] h-[2rem]": "w-[3rem] h-[3rem]"}`} >
             <Image src={getIcon(icon)} alt={name} layout="responsive" />
         </Link>
     );
