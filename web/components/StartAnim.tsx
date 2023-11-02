eda7c8a2e1d135c6b98224a3aba81e56d6f186b0
SayarB <sayarbhattacharyya03@gmail.com>
2023-11-02 15:40:13 +0530

    fix: minor

--- web/components/StartAnim.tsx
+++ web/components/StartAnim.tsx
@@ -3,22 +3,24 @@
 import { Player, PlayerEvent } from "@lottiefiles/react-lottie-player";
 import LoadingAnimation from "../animations/loading_anim.json";
 import LoadingMobile from "../animations/loading_mobile.json"
-import { CSSProperties, useEffect } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 type Props = {
     onComplete: () => void;
     style?: CSSProperties
 }
 const StartAnim = ({ onComplete, style }: Props) => {
-
-    const potraitMode = window.innerWidth < window.innerHeight
+    const [potraitMode, setPotraitMode] = useState(false)
     const aspectPotrait = 416 / 739
     const aspectLandscape = 16 / 9
-
     const aspect = potraitMode ? aspectPotrait : aspectLandscape
-    const screenAspect = innerWidth / innerHeight
-    const dims = { minHeight: potraitMode ? (screenAspect < aspect ? innerHeight + 'px' : undefined) : (screenAspect < aspect ? innerHeight + 'px' : undefined), minWidth: potraitMode ? (screenAspect > aspect ? innerWidth + "px" : undefined) : (screenAspect > aspect ? innerWidth + "px" : undefined) }
-    console.log(aspect, screenAspect, dims)
+    const [screenAspect, setScreenAspect] = useState(16 / 9)
 
+
+    useEffect(() => {
+        if (window) {
+            setPotraitMode(window.innerWidth < window.innerHeight)
+        }
+    }, [window, window.innerWidth, window.innerHeight])
 
 
     useEffect(() => {
@@ -31,7 +33,7 @@ const StartAnim = ({ onComplete, style }: Props) => {
     return <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center overflow-hidden bg-white -z-10" style={style} ><Player
         src={potraitMode ? LoadingMobile : LoadingAnimation}
         autoplay
-        style={{ ...dims, aspectRatio: aspect, zIndex: 0, backgroundColor: "transparent" }}
+        style={{ minHeight: potraitMode ? (screenAspect < aspect ? innerHeight + 'px' : undefined) : (screenAspect < aspect ? innerHeight + 'px' : undefined), minWidth: potraitMode ? (screenAspect > aspect ? innerWidth + "px" : undefined) : (screenAspect > aspect ? innerWidth + "px" : undefined), aspectRatio: aspect, zIndex: 0, backgroundColor: "transparent" }}
 
     /></div>
 }
