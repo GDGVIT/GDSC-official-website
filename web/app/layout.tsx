b36cc38b8725ceae6ed43694115742288bc0e005
anirudh <anirudh04mishra@gmail.com>
2023-11-08 16:00:39 +0530

    feat: gtags and footer

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,11 +1,11 @@
-
-import './globals.css'
-import localFont from "next/font/local"
+import Script from 'next/script';
+import './globals.css';
+import localFont from 'next/font/local';
 
 const neue_machina = localFont({
   src: '/NeueMachina-Regular.otf',
-  variable: '--font-neuemachina'
-})
+  variable: '--font-neuemachina',
+});
 
 export const metadata = {
   title: 'GDSC VIT',
@@ -24,16 +24,40 @@ export const metadata = {
       },
     ],
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${neue_machina.variable} text-[10px] md:text-[16px]`} >
-      <body className='overflow-hidden bg-dark'>{children}</body>
-    </ html>
-  )
+    <html
+      lang="en"
+      className={`${neue_machina.variable} text-[10px] md:text-[16px]`}
+    >
+      <head>
+        <Script id="gtag" strategy="afterInteractive">
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M4WQ5S5N');
+  `}
+        </Script>
+      </head>
+      <body className="overflow-hidden bg-dark">
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4WQ5S5N"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+      `,
+          }}
+        />
+        {children}
+      </body>
+    </html>
+  );
 }
